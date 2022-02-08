55bf8c950d68f7e895972b21b868a026bdac5532
KyleMo <monstadkyle@gmail.com>
2022-02-07 16:58:07 -0800

    nearly built out entire functionality. Biggest accomplishment is successfully knowing when first number to operate on has been initialized and being able to add, subtract multiply or divide several numbers concurrently and still recieving values without pressing equals button.

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 
-let a = 0;
-let b = 0;
+let a = undefined;
+let b = undefined;
 let opperand = "";
+let firstValueDefined = false;
 
 document.getElementById("display-value").innerText = "14";
-// event listner
+// event listners
 document.getElementById("ac-button").addEventListener("click", clearDisplay);
 document.querySelector("#neg-pos").addEventListener("click", () => {makeNegOrPos(getDisplayValue())});
-document.querySelector("#one").addEventListener("click", () => {changeDisplayValue("zero")});
+document.querySelector("#zero").addEventListener("click", () => {changeDisplayValue("zero")});
 document.querySelector("#one").addEventListener("click", () => {changeDisplayValue("one")});
 document.querySelector("#two").addEventListener("click", () => {changeDisplayValue("two")});
 document.querySelector("#three").addEventListener("click", () => {changeDisplayValue("three")});
@@ -17,25 +18,100 @@ document.querySelector("#six").addEventListener("click", () => {changeDisplayVal
 document.querySelector("#seven").addEventListener("click", () => {changeDisplayValue("seven")});
 document.querySelector("#eight").addEventListener("click", () => {changeDisplayValue("eight")});
 document.querySelector("#nine").addEventListener("click", () => {changeDisplayValue("nine")});
-document.querySelector("#add").addEventListener("click", () => {changeDisplayValue("nine")});
+document.querySelector("#point").addEventListener("click", () => {changeDisplayValue("point")});
+//***********
+
+document.querySelector("#add").addEventListener("click", () => {
+  if (a != undefined && opperand != ""){
+    b = getDisplayValue();
+    document.getElementById("display-value").innerText =
+      operate(opperand,a,b);
+    a = operate(opperand,a,b);
+    opperand = "+";
+    b = undefined;
+  }
+  else {
+    a = getDisplayValue();
+    opperand = "+"
+  }
+});
+
+document.querySelector("#subtract").addEventListener("click",() => {
+  if (a != undefined && opperand != ""){
+    b = getDisplayValue();
+    document.getElementById("display-value").innerText =
+      operate(opperand,a,b);
+    a = operate(opperand,a,b);
+    opperand = "-";
+    b = undefined;
+  }
+  else {
+    a = getDisplayValue();
+    opperand = "-";
+  }
+
+});
+
+document.querySelector("#multiply").addEventListener("click",() => {
+  if (a != undefined && opperand != ""){
+    b = getDisplayValue();
+    document.getElementById("display-value").innerText =
+      operate(opperand,a,b);
+    a = operate(opperand,a,b);
+    opperand = "*";
+    b = undefined;
+  }
+  else {
+    a = getDisplayValue();
+    opperand = "*";
+  }
+
+});
+
+document.querySelector("#divide").addEventListener("click",() => {
+  if (a != undefined && opperand != ""){
+    b = getDisplayValue();
+    document.getElementById("display-value").innerText =
+      operate(opperand,a,b);
+    a = operate(opperand,a,b);
+    opperand = "/";
+    b = undefined;
+  }
+  else {
+    a = getDisplayValue();
+    opperand = "/";
+  }
+
+});
+
+document.querySelector("#equals").addEventListener("click", () => {
+  b = getDisplayValue();
+  if (a === undefined){
+    document.getElementById("display-value").innerText = b;
+    b = undefined;
+  }
+  else {
+    document.getElementById("display-value").innerText = operate(opperand,a,b);
+    a = getDisplayValue();
+    b = undefined;
+  }
+});
+
 /*
-document.querySelector("#subtract").addEventListener("click", );
-document.querySelector("#multiply").addEventListener("click", );
-document.querySelector("#divide").addEventListener("click", );
+
 document.querySelector("#percent").addEventListener("click", );
 */
 // button functions
 
 function clearDisplay(){
-  a = 0;
-  b = 0;
+  a = undefined;
+  b = undefined;
   opperand = "";
   document.getElementById("display-value").innerText = "0";
 }
 
 function makeNegOrPos(value){
   if (document.getElementById('display-value').innerText == "0"){
-    console.log("made it here");
     document.getElementById('display-value').innerText = "0";
   }
   else {
@@ -44,50 +120,59 @@ function makeNegOrPos(value){
   }
 }
 
-
 function getDisplayValue(){
-  return document.getElementById('display-value').innerText
+  return Number.parseFloat(document.getElementById('display-value').innerText)
 }
 
-
+function isFirstValueDefined(){
+  if (a != undefined && opperand != ""){
+    return true;
+  }
+  else {
+    return false;
+  }
+}
 
 // Main operation functions
 function changeDisplayValue(buttonID){
-  if (getDisplayValue() === "0"){
-    document.getElementById('display-value').innerText = document.getElementById(buttonID).innerText;
+  if (getDisplayValue().toString.length > 9){
+    document.getElementById('display-value').innerText =
+      getDisplayValue().toString.substring(0,9);
+  }
+
+  if (isFirstValueDefined() && b === undefined) {
+    document.getElementById('display-value').innerText =
+      document.getElementById(buttonID).innerText;
+  }
+  else if (getDisplayValue() === 0){
+    document.getElementById('display-value').innerText =
+      document.getElementById(buttonID).innerText;
   }
   else {
     document.getElementById('display-value').innerText =
       document.getElementById('display-value').innerText +
       document.getElementById(buttonID).innerText;
   }
+
+
+
+
 }
 
 function operate(operator, a, b){
-  if (operate === "+"){
-    return add(a,b);
+  if (operator === "+"){
+    return (a+b);
   }
-  else if (operate === "-"){
-    return subtract(a,b);
+  else if (operator === "-"){
+    return (a-b);
   }
-  else if (operate === "*"){
-    return multiply(a,b);
+  else if (operator === "*"){
+    return (a*b);
   }
-  else if (operate === "/"){
-    return divide(a,b);
+  else if (operator === "/"){
+    if (b == "0" || b == 0){
+      return "Nope"
+    }
+    return (a/b);
   }
-}
-
-function add (a,b){
-  return a+b;
-}
-
-function subtract (a,b){
-  return a-b;
-}
-function multiply (a,b){
-  return a*b;
-}
-function divide (a,b){
-  return a/b;
 }
